2c1b5ff641ba132e6614e83ae867bb9bcb1ee6e2
Glenn Gonda <alohaglenn@icloud.com>
2023-03-01 23:08:32 -0800

    move some logic around

--- internal/tui/social.go
+++ internal/tui/social.go
@@ -84,3 +84,23 @@ var bm = map[social]string{
 func (s social) BaseUrl() string {
 	return bm[s]
 }
+
+type availability bool
+
+func (a availability) Status() status {
+	s := unavailable
+	if a {
+		s = available
+	}
+
+	return s
+}
+
+func (a availability) String() string {
+	s := lipgloss.NewStyle().SetString("𐄂").Foreground(lipgloss.Color("#FF004F")).String()
+	if a {
+		s = lipgloss.NewStyle().SetString("✓").Foreground(lipgloss.Color("#00ffb0")).String()
+	}
+
+	return s
+}
